eb54a4add9060e8b76c3f836fb08171d0cf7cc07
jseban <sebanjulien@gmail.com>
2013-11-21 10:11:54 +0100

    premiere version du search bundle

--- Controller/SearchController.php
+++ Controller/SearchController.php
@@ -18,56 +18,61 @@ use Pagerfanta\Pagerfanta;
 
 class SearchController extends Controller implements SystemController {
 
-    public function filteredSearchAction() {
-        $request = $this->getRequest();
-        $queryString = $request->get('query');
-        $filters = $request->get('filters');
-        
-        $finder = $this->container->get('fos_elastica.finder.majesteel_back');
-
-        $query = new \Elastica\Query\QueryString('admin');
-        
-        $term = new \Elastica\Filter\Term(array('role', true));
-
-        $filteredQuery = new \Elastica\Query\Filtered($query, $term);
-        $resultSet = $finder->find($filteredQuery);
-        
-        var_dump($resultSet);
-        exit;
-        
-        return $this->render('MajesSearchBundle:Search:ajax/list-results.html.twig', array(
-                    'results' => $resultSet));
-    }
-
     public function searchAction() {
         $request = $this->getRequest();
-        $query = $request->get('query');
-        
+        $query = $request->get('query', '*');
+        $filters = $request->get('filters', null);
+
         $index = $this->container->get('fos_elastica.index.majesteel_back');
         $finder = $this->container->get('fos_elastica.finder.majesteel_back');
 
-        //QUERY :
-        $elasticaQuery = new \Elastica\Query();
+        // Define a Query. We want a string query.
         $elasticaQueryString = new \Elastica\Query\QueryString();
+
         $elasticaQueryString->setQuery($query);
+
+        // Create the actual search object with some data.
+        $elasticaQuery = new \Elastica\Query();
         $elasticaQuery->setQuery($elasticaQueryString);
 
-        //FACETS
+        // Pagination
+        $elasticaQuery->setFrom(0);    // Where to start?
+        $elasticaQuery->setLimit(50);   // How many?
+        
+        // Define a new facet.
         $elasticaFacet = new \Elastica\Facet\Terms('tags');
         $elasticaFacet->setField('tags');
-        
-        
+        $elasticaFacet->setSize(10);
+        $elasticaFacet->setOrder('reverse_count');
 
+        // Add that facet to the search query object.
         $elasticaQuery->addFacet($elasticaFacet);
 
-        // ResultSet
-        $resultSet = $finder->find($elasticaQuery);
-        $facets = $index->search($elasticaQuery)->getFacets();
+        // Search and get facets from elasticaResults
+        $elasticaFacets = $index->search($elasticaQuery)->getFacets();
+
+        // Add filters
+        if (!is_null($filters)) {
+            $elasticaFilterOr = new \Elastica\Filter\BoolOr();
+            foreach ($filters as $filterName => $filter) {
+                $elasticaFilter = new \Elastica\Filter\Term();
+                $elasticaFilter->setTerm('tags', $filterName);
+
+                $elasticaFilterOr->addFilter($elasticaFilter);
+            }
+
+            // Set filters
+            $elasticaQuery->setFilter($elasticaFilterOr);
+        }
+
+        //Search on the finder.
+        $elasticaResultSet = $finder->find($elasticaQuery);
 
         return $this->render('MajesSearchBundle:Search:results.html.twig', array(
-                    'results' => $resultSet,
-                    'facets' => $facets,
+                    'results' => $elasticaResultSet,
+                    'facets' => $elasticaFacets,
+                    'query' => $query,
+                    'filters' => $filters,
                     'pageTitle' => 'Search'));
     }
-
 }
